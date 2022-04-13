2bb01dc6ec1aa0df4315549c50ee3fa7593cb207
braedonb98 <brbe3574@colorado.edu>
2022-04-13 12:51:35 -0600

    forgot to stage rest of files from the last commit

--- src/users/pages/Dashboard.js
+++ src/users/pages/Dashboard.js
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div className="Text__Body">
-      <h1>Welcome</h1>
+      <h1>Welcome To Info Speed Manager</h1>
     </div>
   );
 };

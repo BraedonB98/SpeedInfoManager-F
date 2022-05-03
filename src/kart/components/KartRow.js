5838b50f5e4524933ce88802a564dc68d7c0c0ec
braedonb98 <brbe3574@colorado.edu>
2022-05-03 11:39:49 -0600

    creating files for kart tracker modals, added styling to kart tracker page

--- src/kart/components/KartRow.js
+++ src/kart/components/KartRow.js
@@ -13,32 +13,53 @@ const KartRow = (props) => {
   const auth = useContext(AuthContext);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [numberOfKarts, setNumberOfKarts] = useState([]);
+  const [activeKarts, setActiveKarts] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25,
+  ]);
+
+  const openKartSelect = () => {};
+
+  const editRowProperties = () => {};
+
+  const runRow = () => {};
+
   useEffect(() => {
     const startKarts = async () => {
       let row = [];
       for (let i = 0; i < props.kartsInRow; i++) {
         row.push(i);
       }
-      console.log(props.kartsInRow);
       setNumberOfKarts(row.reverse());
     };
     startKarts();
   }, [props.kartsInRow]);
   const karts = numberOfKarts.map((kartPosition) => {
+    //Can turn this into a kart component later instead of <li><Button>
     return (
-      <li className="kart-row__kart-list-item">
-        <Button>{kartPosition}</Button>
+      <li key={kartPosition} className="kart-row__kart-list-item">
+        <Button
+          className="kart-row__kart-list-item-button"
+          onClick={openKartSelect}
+        >
+          {kartPosition}
+        </Button>
       </li>
     );
   });
+
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
-      <Card>
-        <h1>{props.name}</h1>
-        <ul>{karts}</ul>
-        {/*will want to put a input for number of karts to run here*/}
-        <Button>Run Row </Button>
+      <Card className="kart-row__row-div">
+        <Button className="kart-row__row-title" onClick={editRowProperties}>
+          {props.name}
+        </Button>
+        {/*edit row count/active carts*/}
+        <ul className="kart-row__kart-list">{karts}</ul>
+        <Button className="kart-row__row-title" onClick={runRow}>
+          Run Row
+        </Button>
       </Card>
     </React.Fragment>
   );
